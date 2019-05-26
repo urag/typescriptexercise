f51b18f8b1a173129f2467e300b527e7c092f265
unknown <uragaiduk@gmail.com>
2019-05-26 12:29:26 +0300

    WIP adding 1 exercise

---
+++ 1/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const port = 3000;
+
+const reqHanler = (req, res) => {
+    console.log("Got request " + req);
+    sleep(2000);
+    res.end("Hello there !!!");
+}
+
+module.exports.init = function init() {
+    const server = http.createServer(reqHanler);
+    server.listen(port, (err) => {
+        if (err) {
+            return console.log("Error happend", err);
+        }
+
+        console.log("Server is listening on port " + port)
+    })
+}
+
+function sleep(ms) { // node.js >= 9.3  blocks event loop
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+}
